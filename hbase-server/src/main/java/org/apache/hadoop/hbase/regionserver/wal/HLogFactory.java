6a962710b167054db785aaaabd27abf856275857
fpj <fpj@apache.org>
2012-08-13 10:44:08 +0200

    Adding HLogFactory. -F

---
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/wal/HLogFactory.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2010 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.hadoop.hbase.regionserver.wal;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class HLogFactory {
+    private static final Log LOG = LogFactory.getLog(HLogFactory.class);
+    
+    public static HLog getHLog(final FileSystem fs, final Path dir, final Path oldLogDir,
+                                    final Configuration conf) throws IOException {
+        return new FSHLog(fs, dir, oldLogDir, conf);
+    }
+    
+    public static HLog getHLog(final FileSystem fs, final Path dir, final Path oldLogDir,
+                                    final Configuration conf, final List<WALActionsListener> listeners,
+                                    final String prefix) throws IOException {
+        return new FSHLog(fs, dir, oldLogDir, conf, listeners, prefix);
+    }
+}
